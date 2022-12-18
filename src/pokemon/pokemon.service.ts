48a304ea922731fd617133f3360489a9cc6cff03
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-12-18 19:39:03 -0300

    se optimiza delete by id

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -78,8 +78,16 @@ export class PokemonService {
   async remove(id: string) {
     // const pokemon = await this.findOne(id);
     // await pokemon.deleteOne();
-    const result = this.pokemonModel.findByIdAndDelete(id);
-    return result;
+    // const result = await this.pokemonModel.findByIdAndDelete(id);
+    const { deletedCount } = await this.pokemonModel.deleteOne({
+      _id: id,
+    });
+
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+    }
+
+    return;
   }
 
   private handleException(error: any) {
