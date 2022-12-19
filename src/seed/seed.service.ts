74d8711eb0583fa5d5c687c26e858a20e4cf3615
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-12-19 14:30:31 -0300

    consumir pokemones desde pokeapi

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  excuteSeed() {
-    return 'This action adds a new seed';
+  private readonly axios: AxiosInstance = axios; //dependecia de axios
+
+  async excuteSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'http://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2]; // penultima posicion
+      console.log({ name, no });
+    });
+
+    return data.results;
   }
 }
