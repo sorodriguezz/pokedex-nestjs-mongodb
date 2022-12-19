de61d3491c51003392d8c2e4c85e3069237c3cf6
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-12-19 14:09:35 -0300

    se instala axios y se crea servicio seed

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -29,7 +29,7 @@ export class PokemonService {
   }
 
   findAll() {
-    return `This action returns all pokemon`;
+    return this.pokemonModel.find();
   }
 
   async findOne(term: string) {
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SeedService {
+  excuteSeed() {
+    return 'This action adds a new seed';
+  }
+}
