0abeb9b37fd115fd29fb53943e35d0508758a187
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-12-19 14:40:16 -0300

    import y export module

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios; //dependecia de axios
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async excuteSeed() {
     const { data } = await this.axios.get<PokeResponse>(
@@ -14,7 +22,6 @@ export class SeedService {
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2]; // penultima posicion
-      console.log({ name, no });
     });
 
     return data.results;
