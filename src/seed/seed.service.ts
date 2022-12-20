1c0f62460aa7090fa786e2f21f9d1830faac8b61
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-12-20 12:31:57 -0300

    Patron adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
-import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
-import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios; //dependecia de axios
 
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async excuteSeed() {
     await this.pokemonModel.deleteMany({}); // DELETE * FROM POKEMON
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'http://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
